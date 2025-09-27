3a7ca14b14549eef48ceb16ab57199df7ae34fe6
r.luchinsky <r.v.luchinskiy@tinkoff.ru>
2025-09-27 21:57:58 +0300

    add key map for jsdtls

--- lua/configs/jdtls.lua
+++ lua/configs/jdtls.lua
@@ -30,21 +30,29 @@ vim.api.nvim_create_autocmd("FileType", {
                 }
             },
             init_options = { bundles = {} },
+
             on_attach = function(client, bufnr)
                 -- Настройки табов
                 vim.bo[bufnr].expandtab = true
                 vim.bo[bufnr].shiftwidth = 4
                 vim.bo[bufnr].tabstop = 4
                 vim.bo[bufnr].softtabstop = 4
-
-                -- Горячие клавиши LSP
-                local opts = { noremap=true, silent=true, buffer=bufnr }
+            
+                -- Общие опции для маппингов
+                local opts = { noremap = true, silent = true, buffer = bufnr }
+            
+                -- LSP
+                vim.keymap.set('n', '<Leader>ca', vim.lsp.buf.code_action, opts)
+                vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, opts)
                 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+                vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
                 vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-                vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-                vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format({ async = true }) end, opts)
-            end
-        })
+                vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+                vim.keymap.set('n', '<Leader>f', function() vim.lsp.buf.format({ async = true }) end, opts)
+            end       
+       })
     end
 })
 
